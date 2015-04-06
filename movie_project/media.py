aefdc927863369d5e230a3e16a30b8454c8ecfd1
nambw <namratawadhwa@yahoo.com>
2015-04-05 18:46:34 -0700

    Committed by Udacity

---
+++ movie_project/media.py
@@ -0,0 +1,12 @@
+import webbrowser
+
+class Movie():
+	def __init__(self, m_title, m_story,  m_poster, m_link):
+		self.title = m_title
+		self.storyline = m_story
+		self.poster_image_url = m_poster
+		self.trailer_youtube_url = m_link
+
+'''     	def show_trailer(self):
+		webbrowser.open(self.trailer_youtube_url) '''
+
